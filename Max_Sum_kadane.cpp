edd48ae68a71f4b8b5ebb51923d11c174f3712c8
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-08-29 12:05:13 +0530

    Create Max_Sum_kadane.cpp

---
+++ Max_Sum_kadane.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int max_sum( int arr[], int n ){
+
+    int localSum=arr[0];
+    int globalSum=arr[0];
+    for(int i=1; i<n;i++){
+        
+        localSum=max(arr[i], localSum+arr[i]);
+        globalSum=max(localSum,globalSum);
+
+    }
+    return globalSum;
+}
