85ba7f7742bc9ddbfbdc910e45f35ac375864c50
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-09-03 19:24:20 +0530

    Update 05_Find_number_of_occurrence _in_sorted_array.cpp

--- Binary Search/05_Find_number_of_occurrence _in_sorted_array.cpp
+++ Binary Search/05_Find_number_of_occurrence _in_sorted_array.cpp
@@ -1,3 +1,51 @@
 //Find number of occurrence in a sorted array
 //It will be same as first occurrence - last occurrence + 1
 
+int first_occurrence(int arr[], int l, int r, int key){
+
+    int res=-1;
+    while(r>=l){
+        int mid= l + (r-l)/2;
+        if ( arr[mid] == key){
+                res=mid;
+                r=mid-1;
+        }
+        else if(key < arr[mid])
+                r=mid-1;
+        else
+                l=mid+1;
+
+
+    }
+    return res;
+}
+
+int last_occurrence(int arr[], int l, int r, int key){
+
+    int res=-1;
+    while(r>=l){
+        int mid= l + (r-l)/2;
+        if ( arr[mid] == key){
+                res=mid;
+                l=mid+1;
+        }
+        else if(key < arr[mid])
+                r=mid-1;
+        else
+                l=mid+1;
+
+
+    }
+    return res;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 5,5,5,5, 6, 7, 8, 9, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 5;
+    cout<<last_occurrence(arr,0,n-1,key)-first_occurrence(arr,0,n-1,key)+1;
+   return 0;
+}
+
+
+
