850d4f30dc139de6b89d8d019183f7ccb732039b
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-09-08 09:18:21 +0530

    Update 17_Maximum_sum_of_among_all_rotations_of_a_given_array.cpp

--- 17_Maximum_sum_of_among_all_rotations_of_a_given_array.cpp
+++ 17_Maximum_sum_of_among_all_rotations_of_a_given_array.cpp
@@ -15,3 +15,30 @@ int MaxSum(int arr[], int n){
     }
     return res;
 }
+
+
+//Efficient approach
+//Time O(n)
+//next_val = curr_val - (cum_sum - arr[i-1]) + arr[i-1] * (n-1);
+int MaxSum(int arr[], int n){
+
+
+
+    int sum=0;
+
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+
+    int curr_val=0;
+    for(int i=0;i<n;i++)
+        curr_val+=arr[i]*i;
+
+    int res=curr_val;
+
+    for(int i=1;i<n;i++){
+        int next_val=curr_val-(sum-arr[i-1]) + arr[i-1]*(n-1);
+        curr_val=next_val;
+        res=max(res,next_val);
+    }
+        return res;
+}
